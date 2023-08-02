eb577d8240c696276bae19e0ab2eb0b3e9e5708b
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-02 00:17:24 -0400

    Se construyó el formulario de selección de categorías

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { Container, Grid, Typography } from '@mui/material'
+import Formulario from './components/Formulario'
+
 function App() {
 
   return (
-    <h1>App Noticias</h1>
+      <Container>
+          <header>
+            <Typography align='center' marginY={5} component="h1" variant="h3">
+              Buscador de Noticias
+            </Typography>
+          </header>
+
+          <Grid 
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={10} md={6} lg={4}>
+              <Formulario />
+            </Grid>
+          </Grid>
+
+
+          <h2>listado noticias</h2>
+
+
+      </Container>
   )
 }
 
